fd5a2c9a2d8208de8a811aa59dd15a02b635e185
xireiki <i@xireiki.com>
2024-10-03 18:03:11 +0800

    chore: .gitignore 不会在构建进模块

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$@" ]; then
-  zip -r -o -X wifi_switch_$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'update.json'
+  zip -r -o -X wifi_switch_$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'update.json' -x '.gitignore'
 else
-  zip -r -o -X wifi_switch_${1}.zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'update.json'
+  zip -r -o -X wifi_switch_${1}.zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'update.json' -x '.gitignore'
 fi
