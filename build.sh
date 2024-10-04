b18fcfc1b1b9971d17d1f8e90bbfe8596644345f
xireiki <i@xireiki.com>
2024-10-04 15:03:02 +0800

    chore: build.sh 不会再将 wifi_switch_*.zip 打包

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$@" ]; then
-  zip -r -o -X wifi_switch_$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'update.json' -x '.gitignore'
+  zip -r -o -X wifi_switch_$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'update.json' -x '.gitignore' -x 'wifi_switch_*.zip'
 else
-  zip -r -o -X wifi_switch_${1}.zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'update.json' -x '.gitignore'
+  zip -r -o -X wifi_switch_${1}.zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'update.json' -x '.gitignore' -x 'wifi_switch_*.zip'
 fi
